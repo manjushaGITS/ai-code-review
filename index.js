6485d7fbdbb7e8fdf31bed405131e9261de9ba0e
manjushaGITS <49211798+manjushaGITS@users.noreply.github.com>
2024-10-18 18:04:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function GREETMASTER(name) {
   return greeting;
 }
 GREETMASTER();
+
+function renderButton() {
+  const buttonEle = document.createElement('button');
+  button.textContent = "click here";
+  document.body.appendChild(buttonEle);
+}
